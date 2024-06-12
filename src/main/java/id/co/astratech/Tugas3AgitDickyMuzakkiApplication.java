04f5f37923e6db52277a8f90c96aa479d50b7d57
Dicky Muzakki <dickymuzakki05@gmail.com>
2024-06-12 23:56:35 +0700

    commit ke 11 haha

---
+++ src/main/java/id/co/astratech/Tugas3AgitDickyMuzakkiApplication.java
@@ -0,0 +1,13 @@
+package id.co.astratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas3AgitDickyMuzakkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tugas3AgitDickyMuzakkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/astratech/Tugas3AgitDickyMuzakkiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.astratech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas3AgitDickyMuzakkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
